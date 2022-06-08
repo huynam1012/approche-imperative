09788eea7aab9a6ac951e864075101fd1f84599c
hntran <huynam1012@gmail.com>
2022-06-08 14:42:47 +0200

    2e commit

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -1,5 +1,25 @@
 package fr.declaration.variable;
 
 public class DeclarationApp {
+	
+	public static void main(String[] args) {
 
+		byte b = 10;
+		short s = -17282;
+		int i = 2846325;
+		long l = 1000L;
+		float f = 2.5F;
+		double d = 6.4D;
+		char c = 'c';
+		boolean myBool = true;
+		
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(myBool);
+	}
 }
